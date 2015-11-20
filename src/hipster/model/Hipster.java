c8906802aef1191995c2f35e23c5bdb22c5c4a44
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-11-20 12:13:19 -0700

    General code clean up

--- src/hipster/model/Hipster.java
+++ src/hipster/model/Hipster.java
@@ -4,17 +4,60 @@ public class Hipster
 {
 	private String name;
 	private String[] hipsterPhrases;
-	private Book [] hipsterBooks;
-	
-	
+	private Book[] hipsterBooks;
+
 	public Hipster()
 	{
 		this.name = "";
 		this.hipsterPhrases = new String[4];
 		this.hipsterBooks = new Book[3];
-		
-		
+
 		setupArray();
+		setupBooks();
+	}
+
+	private void setupArray()
+	{
+		hipsterPhrases[0] = "That is so mainstream!";
+		hipsterPhrases[1] = "I don't mean to sound like a hipster , but ";
+		hipsterPhrases[2] = "OH NICE JOB";
+		hipsterPhrases[3] = "I was into computers before it was cool";
+	}
+
+	private void setupBooks()
+	{
+		Book firstBook, seconedBook, thirdBook;
+
+		firstBook = new Book();
+		firstBook.setAuthor("Bill nye");
+		firstBook.setTitle("Undeniable");
+		firstBook.setSubject("Evolutionary science");
+		firstBook.setPageCount(350);
+		firstBook.setPrice(20.00);
+
+		seconedBook = new Book();
+		seconedBook.setAuthor("Bill nye");
+		seconedBook.setTitle("UNSTOPABLE");
+		seconedBook.setSubject("Climate Change");
+		seconedBook.setPageCount(400);
+		seconedBook.setPrice(30.00);
+
+		thirdBook = new Book(5, "Dr.Suess", "Counting", "one fish two fish", 2.00);
+
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = seconedBook;
+		hipsterBooks[2] = thirdBook;
+
+	}
+
+	public Book[] getHipsterBooks()
+	{
+		return hipsterBooks;
+	}
+
+	public void setHipsterBooks(Book[] hipsterBooks)
+	{
+		this.hipsterBooks = hipsterBooks;
 	}
 
 	public String getName()
@@ -37,54 +80,4 @@ public class Hipster
 		this.hipsterPhrases = hipsterPhrases;
 	}
 
-	private void setupArray()
-	{
-		hipsterPhrases[0] = "That is so mainstream!";
-		hipsterPhrases[1] = "I don't mean to sound like a hipster , but ";
-		hipsterPhrases[2] = "OH NICE JOB";
-		hipsterPhrases[3] = "I was into computers before it was cool";
-	}
-
-	private void setupBooks()
-	{
-		Book firstBook, seconedBook, thirdBook;
-		
-		firstBook = new Book();
-		firstBook.setAuthor("Bill nye");
-		firstBook.setTitle("Undeniable");
-		firstBook.setSubject("Evolutionary science");
-		firstBook.setPageCount(350);
-		firstBook.setPrice(20.00);
-		
-		
-		seconedBook = new Book();
-		
-		seconedBook.setAuthor("Bill nye");
-		seconedBook.setTitle("UNSTOPABLE");
-		seconedBook.setSubject("Climate Change");
-		seconedBook.setPageCount(400);
-		seconedBook.setPrice(30.00);
-		
-		thirdBook = new Book(5, "Dr.Suess", "Counting", "one fish two fish", 2.00 );
-		
-		hipsterBooks[0] = firstBook;
-		hipsterBooks[1] = seconedBook;
-		hipsterBooks[2] = thirdBook;
-		
-		
-		
-		
-	}
-
-	public Book[] getHipsterBooks()
-	{
-		return hipsterBooks;
-	}
-
-	public void setHipsterBooks(Book[] hipsterBooks)
-	{
-		this.hipsterBooks = hipsterBooks;
-	}
-	
-	
 }
--- src/hipster/view/HipsterPanel.java
+++ src/hipster/view/HipsterPanel.java
@@ -23,10 +23,6 @@ public class HipsterPanel extends JPanel
 	private JButton changeBookButton;
 	private int maxClicks;
 	private int startClick;
-	
-	
-	
-	
 
 	public HipsterPanel(HipsterController baseController)
 	{
@@ -40,8 +36,8 @@ public class HipsterPanel extends JPanel
 		changeBookButton = new JButton("Change books");
 		phraseComboBox = new JComboBox<String>();
 		maxClicks = baseController.getFirstHipster().getHipsterBooks().length;
-		
-		
+		startClick = 0;
+
 		setupComboBox();
 		setupPanel();
 		setupLayout();
@@ -61,12 +57,31 @@ public class HipsterPanel extends JPanel
 		this.setLayout(baseLayout);
 		this.setBackground(Color.YELLOW);
 		this.add(phraseComboBox);
+		this.add(bookPageCountLabel);
+		this.add(bookAuthorLabel);
+		this.add(bookPriceLabel);
+		this.add(bookSubjectLabel);
+		this.add(bookTitleLabel);
+		this.add(changeBookButton);
 
 	}
 
 	private void setupLayout()
 	{
-
+		baseLayout.putConstraint(SpringLayout.SOUTH, bookAuthorLabel, -204, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, bookAuthorLabel, -176, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPageCountLabel, 6, SpringLayout.SOUTH, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookPageCountLabel, 0, SpringLayout.WEST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPriceLabel, 6, SpringLayout.SOUTH, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookPriceLabel, 0, SpringLayout.WEST, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookSubjectLabel, 6, SpringLayout.SOUTH, bookPriceLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookSubjectLabel, 0, SpringLayout.WEST, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookTitleLabel, 5, SpringLayout.SOUTH, bookSubjectLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, bookTitleLabel, 0, SpringLayout.WEST, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, changeBookButton, 75, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, changeBookButton, 33, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, phraseComboBox, 25, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, phraseComboBox, -34, SpringLayout.EAST, this);
 	}
 
 	private void setupListeners()
@@ -83,46 +98,34 @@ public class HipsterPanel extends JPanel
 				baseController.getBaseFrame().setTitle(updatedTitle);
 			}
 		});
-			
+
 		changeBookButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent click)
 			{
-				Book [] tempBooks = baseController.getFirstHipster().getHipsterBooks();
-				if(startClick < maxClicks)
+				Book[] tempBooks = baseController.getFirstHipster().getHipsterBooks();
+				if (startClick < maxClicks)
 				{
 					bookSubjectLabel.setText("Book subject: " + tempBooks[startClick].getSubject());
-					bookAuthorLabel.setText("Book author: " + tempBooks[startClick].getSubject());
-					bookTitleLabel.setText("Book title: " + tempBooks[startClick].getSubject());
-					bookPageCountLabel.setText("Book pages: " + tempBooks[startClick].getSubject());
-					bookPriceLabel.setText("Book price " + tempBooks[startClick].getSubject());
+					bookAuthorLabel.setText("Book author: " + tempBooks[startClick].getAuthor());
+					bookTitleLabel.setText("Book title: " + tempBooks[startClick].getTitle());
+					bookPageCountLabel.setText("Book pages: " + tempBooks[startClick].getPageCount());
+					bookPriceLabel.setText("Book price " + tempBooks[startClick].getPrice());
+					startClick++;
 				}
 				else
 				{
 					startClick = 0;
 					bookSubjectLabel.setText("Book subject: ");
-					bookAuthorLabel.setText("Book author" );
-					bookTitleLabel.setText("Book title: " );
-					bookPageCountLabel.setText("Book pages: " );
-					bookPriceLabel.setText("Book price " );
-				
-				
-				
-				
+					bookAuthorLabel.setText("Book author");
+					bookTitleLabel.setText("Book title: ");
+					bookPageCountLabel.setText("Book pages: ");
+					bookPriceLabel.setText("Book price ");
+
 				}
 			}
 		});
-	
-	
-	
-	
-	
-	
-	
+
 	}
-	
-	
-	
-	
 
 }
